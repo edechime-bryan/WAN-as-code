d8476a80936c4f031aceba3f25c1adc64553039d
edechime-bryan <edechimebryan@gmail.com>
2026-02-21 14:27:58 +0100

    Construct 3 VPCs with module

--- main.tf
+++ main.tf
@@ -24,3 +24,79 @@ provider "aws" {
   alias  = "paris"
   region = "eu-west-3"
 }
+
+module "ohio-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "ohio-vpc"
+  cidr = "10.11.53.0/24"
+
+  azs             = ["us-east-2a"]
+  private_subnets = ["10.0.1.0/26"]
+  public_subnets  = ["10.0.101.128/26"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "tokyo-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "tokyo-vpc"
+  cidr = "192.168.16.0/24"
+
+  azs             = ["ap-northeast-1a"]
+  private_subnets = ["192.168.16.0/26"]
+  public_subnets  = ["192.168.16.128/26"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "paris-vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "paris-vpc"
+  cidr = "172.21.0.0/16"
+
+  azs             = ["eu-west-3a"]
+  private_subnets = ["172.21.0.0/17"]
+  public_subnets  = ["172.21.128.0/17"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+module "ohio-tokyo-peering" {
+  source = "grem11n/vpc-peering/aws"
+
+  providers = {
+    aws.this = aws.ohio
+    aws.peer = aws.tokyo
+  }
+
+  this_vpc_id = var.ohio_vpc_id
+  peer_vpc_id = var.tokyo_vpc_id
+
+  auto_accept_peering = true
+
+  tags = {
+    Name        = "ohio-tokyo-peering"
+    Environment = "Test"
+  }
+}
